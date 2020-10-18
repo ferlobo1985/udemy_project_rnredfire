60cab805954f40f82a38f8be324274292d612e70
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-18 02:48:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements'
 
-export default function App() {
+const App = ()=>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <Text>Hello</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/store/reducers/articles_reducer.js
@@ -0,0 +1,13 @@
+import { act } from "react-test-renderer";
+
+const INITIAL_STATE = {
+    user:null,
+    isAuth:false
+}
+
+export default function(state=INITIAL_STATE,action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
