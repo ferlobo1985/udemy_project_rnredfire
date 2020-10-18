b5cf4cc80c8d5cc39a822d38d58cd4611d1b9a40
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-18 03:06:51 -0300

    dispatching an action

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from 'react-native-elements'
+import { Header } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
+import { getArticles } from './src/store/actions';
 
 const App = ()=>{
+  const articles = useSelector(state => state.articles);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getArticles())
+  },[dispatch])
+
   return (
     <View>
       <Header
--- src/store/reducers/articles_reducer.js
+++ src/store/reducers/articles_reducer.js
@@ -7,6 +7,8 @@ const INITIAL_STATE = {
 
 export default function(state=INITIAL_STATE,action){
     switch(action.type){
+        case 'GET_ARTICLES':
+            return {...state, posts: action.payload }
         default:
             return state;
     }
